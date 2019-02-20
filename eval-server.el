4ba74f3f707da590fd12d527a5f255b802834d95
Lars Ingebrigtsen <larsi@gnus.org>
2019-02-20 13:03:19 +0100

    Clean up the code and rename internal functions.

--- eval-server.el
+++ eval-server.el
@@ -26,47 +26,60 @@
 ;; (push "~/src/eval-server.el" load-path)
 ;; (autoload 'eval-server-start "eval-server" nil t)
 
+;; To test:
+
+;; (start-eval-server "lights" 8710 '(+))
+;; (eval-at "lights" "stories" 8710 '(+ 1 2))
+;;
+;; ~/.authinfo:
+;; machine lights port 8710 password secret
+
 ;;; Code:
 
-(defvar eval-server-processes nil)
+(defvar eval-server--processes nil)
 
-(defun eval-server-start (name port functions)
-  "Start server NAME listening to PORT accepting FUNCTIONS."
-  (let ((server (assq port eval-server-processes)))
+(defun start-eval-server (name port functions)
+  "Start server NAME listening to PORT accepting FUNCTIONS.
+
+If a server is already listening to PORT, it is deleted first."
+  (let ((server (assq port eval-server--processes)))
     (when server
       (delete-process (cdr server))
-      (setq eval-server-processes (delq server eval-server-processes))))
+      (setq eval-server--processes (delq server eval-server--processes))))
   (let ((auth (car (auth-source-search :max 1 :port port :host name))))
     (unless auth
       (error "Couldn't find encryption secret in ~/.authinfo"))
     (push (cons port
 		(make-network-process
 		 :name name
-		 :buffer (get-buffer-create "*eval-server*")
+		 :buffer (get-buffer-create " *eval-server*")
 		 :family 'ipv4
 		 :service port
 		 :host (system-name)
 		 :filter-multibyte nil
 		 :filter (lambda (proc string)
-			   (eval-server-filter
+			   (eval-server--filter
 			    proc auth string functions))
-		 :sentinel 'eval-server-sentinel
+		 :sentinel 'eval-server--sentinel
 		 :server t))
-	  eval-server-processes))
-  (message "Listening on port %s" port))
+	  eval-server--processes))
+  (message "Server %s listening on port %s" name port))
 
 (defun eval-at (name host port form)
+  "Connect to HOST:PORT and eval FORM there.
+NAME is used to find the encryption password from your password
+store, which may be ~/.authinfo."
   (with-temp-buffer
     (set-buffer-multibyte nil)
     (let ((proc
 	   (open-network-stream (format "eval-at-%s" host) (current-buffer)
 				host port))
 	  (auth (car (auth-source-search :max 1 :port port :host name))))
+      ;; Ignore any signals.
       (set-process-sentinel proc (lambda (&rest _)))
-      (with-temp-buffer
-	(set-buffer-multibyte nil)
-	(insert (format "%S\n" (eval-server--encrypt-form auth form)))
-	(process-send-region proc (point-min) (point-max)))
+      (process-send-string
+       proc (format "%S\n" (eval-server--encrypt-form auth form)))
+      ;; Wait until we get a full response back.
       (while (and (process-live-p proc)
 		  (not (search-forward "\n" nil t)))
 	(accept-process-output proc 0 10))
@@ -75,26 +88,29 @@
       (and (plusp (buffer-size))
 	   (eval-server--decrypt-command auth (read (current-buffer)))))))
 
-(defvar eval-server-clients nil)
+(defvar eval-server--clients nil)
 
-(defun eval-server-filter (proc auth string functions)
-  (let ((client (assq proc eval-server-clients)))
+(defun eval-server--filter (proc auth string functions)
+  (let ((client (assq proc eval-server--clients)))
     (unless client
       (setq client (cons proc ""))
-      (push client eval-server-clients))
+      (push client eval-server--clients))
+    ;; Data may come in incomplete packets.  Don't try to parse
+    ;; anything until we've gotten a newline.
     (let ((message (concat (cdr client) string)))
       (if (string-match "\n\\'" message)
 	  (progn
-	    (eval-server-dispatch proc auth message functions)
+	    (eval-server--dispatch proc auth message functions)
 	    (delete-process (car client))
-	    (eval-server-remove proc))
+	    (eval-server--remove proc))
+	;; Add this incomplete package to the cache.
 	(setcdr client message)))))
 
-(defun eval-server-dispatch (proc auth command functions)
+(defun eval-server--dispatch (proc auth command functions)
   (let ((command (eval-server--decrypt-command
 		  auth (ignore-errors
 			 (car (read-from-string command))))))
-    (eval-server-reply
+    (eval-server--reply
      proc auth 
      (if (and command
 	      (consp command)
@@ -104,19 +120,19 @@
        (format "Invalid command %s" command)))
     (process-send-eof proc)))
 
-(defun eval-server-reply (proc auth form)
-  (with-temp-buffer
-    (insert (format "%S\n" (eval-server--encrypt-form auth form)))
-    (process-send-region proc (point-min) (point-max))))
+(defun eval-server--reply (proc auth form)
+  (process-send-string
+   proc
+   (format "%S\n" (eval-server--encrypt-form auth form))))
 
-(defun eval-server-sentinel (proc message)
-  (when (string= message "connection broken by remote peer\n")
-    (eval-server-remove proc)))
+(defun eval-server--sentinel (proc message)
+  (when (equal message "connection broken by remote peer\n")
+    (eval-server--remove proc)))
 
-(defun eval-server-remove (proc)
-  (setq eval-server-clients (assq-delete-all proc eval-server-clients)))
+(defun eval-server--remove (proc)
+  (setq eval-server--clients (assq-delete-all proc eval-server--clients)))
 
-(defun eval-server-pad (s length)
+(defun eval-server--pad (s length)
   "Pad string S to a modulo of LENGTH."
   (concat (make-string (- length (mod (length s) length)) ?\s)
 	  s))
@@ -129,15 +145,17 @@ The encrypted result and the IV are returned."
       (error "Cipher %s isn't supported" cipher))
     (gnutls-symmetric-encrypt
      cipher
-     (eval-server-pad secret (plist-get cdata :cipher-keysize))
+     (eval-server--pad secret (plist-get cdata :cipher-keysize))
      (list 'iv-auto (plist-get cdata :cipher-ivsize))
      (eval-server--pkcs7-pad message (plist-get cdata :cipher-blocksize)))))
 
 (defun eval-server--pkcs7-pad (string length)
+  "Perform PKCS#7 padding to STRING."
   (let ((pad (- length (mod (length string) length))))
     (concat string (make-string pad pad))))
 
 (defun eval-server--pkcs7-unpad (string)
+  "Remove PKCS#7 padding from STRING."
   (substring string 0 (- (length string)
 			 (aref string (1- (length string))))))
 
@@ -147,7 +165,7 @@ The encrypted result and the IV are returned."
       (error "Cipher %s isn't supported" cipher))
     (gnutls-symmetric-decrypt
      cipher
-     (gnutls-pad secret (plist-get cdata :cipher-keysize))
+     (eval-server--pad secret (plist-get cdata :cipher-keysize))
      iv
      encrypted)))
 
